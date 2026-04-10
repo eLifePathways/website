5b2841aedbcc49e645a875de50f374f98022380e
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2026-04-10 11:25:06 +0100

    Update to match new domain #8

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: 'https://elifepathways.github.io',
-  base: '/website',
+  site: "https://elifepathways.org",
   devToolbar: { enabled: false },
   vite: {
     server: {
-      allowedHosts: ['host.docker.internal']
-    }
-  }
+      allowedHosts: ["host.docker.internal"],
+    },
+  },
 });
