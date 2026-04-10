e51ce21254e634b7bec8bbe72c08f6c0472967be
LinaKind <l.kinduryte@elifesciences.org>
2026-04-10 13:09:54 +0300

    Fix path references #8

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://elifepathways.github.io',
+  base: '/website',
   devToolbar: { enabled: false },
   vite: {
     server: {
