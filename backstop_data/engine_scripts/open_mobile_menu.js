64cf8af516c800a6728cd819f1a9d29a383eda85
David Moulton <d.moulton@elifesciences.org>
2026-04-15 11:02:36 +0100

    Lint backstop-related files using Prettier #10

--- backstop_data/engine_scripts/open_mobile_menu.js
+++ backstop_data/engine_scripts/open_mobile_menu.js
@@ -1,6 +1,6 @@
 module.exports = async (page, scenario, vp) => {
   await page.evaluate(() => {
-    window.location.hash = 'mobile-nav';
+    window.location.hash = "mobile-nav";
   });
-  await new Promise(resolve => setTimeout(resolve, 300));
+  await new Promise((resolve) => setTimeout(resolve, 300));
 };
